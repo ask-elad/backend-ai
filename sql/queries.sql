e9adf843924ab316f0c90558a715637f86bed3fd
Aryan Rai <aryanrai746@gmail.com>
2026-08-19 17:14:23 +0530

    Stage 4: explored SQLite

---
+++ sql/queries.sql
@@ -0,0 +1,13 @@
+SELECT * FROM tasks;
+
+-- Show only completed tasks
+SELECT * FROM tasks WHERE done = 1;
+
+-- Count all tasks
+SELECT COUNT(*) FROM tasks;
+
+-- Mark every task as completed
+UPDATE tasks SET done = 1;
+
+-- Delete all completed tasks
+DELETE FROM tasks WHERE done = 1;
